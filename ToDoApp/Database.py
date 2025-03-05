d5afd0aa1353d0f963913e6f0a87ce9dfdf094e6
BuiMinhWin <bmthang2004@gmail.com>
2025-03-05 09:52:19 +0700

    Production database change

--- ToDoApp/Database.py
+++ ToDoApp/Database.py
@@ -4,6 +4,8 @@ from sqlalchemy.ext.declarative import  declarative_base
 
 # SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:12345@localhost/TodoApplicationDatabase'
 SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:12345@127.0.0.1:3306/todoapplicationdatabase'
+SQLALCHEMY_DATABASE_URL = 'postgresql://deploy_server_user:TdFNtEZzY8zVQbmZCAz4JnILMgdozRh3@dpg-cv3rn8qj1k6c73crc6dg-a.oregon-postgres.render.com/deploy_server'
+
 
 engine =  create_engine(SQLALCHEMY_DATABASE_URL)
 
